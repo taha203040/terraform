006d2578e870a5adbea8641a4df96dce83b0e9a6
taha <jokerdz380@gmail.com>
2026-08-22 00:07:54 +0100

    new configuration to upload a image

--- main.tf
+++ main.tf
@@ -14,4 +14,8 @@ resource "aws_s3_bucket_public_access_block" "my_bucket_public_access_block" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
-
+resource "aws_s3_object" "image" {
+  bucket = aws_s3_bucket.my_bucket.id # Reference the bucket ID
+  key    = "image.png" # Path in the bucket
+  source = "image.png" # Local file path
+}
